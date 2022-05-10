639c4f08cdb078c5c03b2a7d10d45d52f42e8fcc
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-05-10 16:37:56 -0500

    Up to model binding

---
+++ MVCDemoThing/Controllers/PeopleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemoThing.Data;
+using MVCDemoThing.Models;
+
+namespace MVCDemoThing.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // [Route("/people/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // [Route("/people/form")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        // handle this post request that is getting sent to /lmao by the form
+        // display list of people after having added a person
+        // render the people/index view!
+        [HttpPost]
+        [Route("/lmao")]
+        public IActionResult AddPerson(string name, string color1, string color2)
+        {
+            // let's create a new instance of Person using the 3 pieces of information from the form
+            Person newPerson = new Person(name, color1, color2);
+
+            // now we want to be able to store our newPerson object somewhere to be used later in our application
+            // instead of just being dropped into the ViewBag and then being forgotten since ViewBag is kind of this one way transaction
+            PersonData.Add(newPerson);
+
+            // Now we can drop the people list into the ViewBag, and then loop through the list to display all of the people in the list
+            ViewBag.people = PersonData.people;
+            
+
+            // let's take the name, color1, and color2 data and make it accessible by the Index view
+            // we are rendering with View("Index")
+            //ViewBag.name = name;
+            //ViewBag.color1 = color1;
+            //ViewBag.color2 = color2;
+
+            return View("Index");
+        }
+    }
+}
---
+++ MVCDemoThing/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCDemoThing.Models
+{
+    public class Person
+    {
+        // name, color1, color2
+        public string Name { get; set; }
+        public string Color1 { get; set; }
+        public string Color2 { get; set; }
+
+        public Person(string name, string color1, string color2)
+        {
+            Name = name;
+            Color1 = color1;
+            Color2 = color2;
+        }
+    }
+}
