7c5e242a01eb6a5f8e3a85836b4bdb87974b4d1b
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-05-13 10:09:03 -0500

    Implemented AddPersonViewModel

--- MVCDemoThing/Controllers/PeopleController.cs
+++ MVCDemoThing/Controllers/PeopleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCDemoThing.Data;
 using MVCDemoThing.Models;
+using MVCDemoThing.ViewModels;
 
 namespace MVCDemoThing.Controllers
 {
@@ -27,10 +28,23 @@ namespace MVCDemoThing.Controllers
         // render the people/index view!
         [HttpPost]
         [Route("/people/form")]
-        public IActionResult AddPerson(Person newPerson)
+        public IActionResult AddPerson(AddPersonViewModel viewModel)
         {
+            //if (viewModel.Name == null) { etc...} 
+            //if (viewModel.Color1 == null) { etc... }
+            // if the model state is not valid, ie viewModel.Name and viewModel.Color1 are null, then 
+            // re-render the form page
+            if (!ModelState.IsValid)
+            {
+                return View("Form");
+            }
+
+            // if model state IS valid, then we can move past that if statement and execute the rest of our code as normal
+            // Which means creating a new instance of Person (a new Person object) and adding it into our PersonData data layer etc....
+
             // let's create a new instance of Person using the 3 pieces of information from the form
-            //Person newPerson = new Person(name, color1, color2);
+            // Person newPerson = new Person(name, color1, color2);
+            Person newPerson = new Person(viewModel.Name, viewModel.Color1, viewModel.Color2);
 
             // now we want to be able to store our newPerson object somewhere to be used later in our application
             // instead of just being dropped into the ViewBag and then being forgotten since ViewBag is kind of this one way transaction
---
+++ MVCDemoThing/ViewModels/AddPersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCDemoThing.ViewModels
+{
+    public class AddPersonViewModel
+    {
+        // We want Name and Color1 to be required fields, but Color2 is optional
+
+        [Required(ErrorMessage = "You didn't put in a name, silly!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You need to provide at least one favorite color!")]
+        public string Color1 { get; set; }
+
+        public string Color2 { get; set; }
+    }
+}
